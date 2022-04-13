9b8131e37574b3a8804ae31ee3ca756782ac2dd1
OlenaPustova <i.am.elena.p@gmail.com>
2022-04-13 15:40:51 +0300

    homework

--- src/index.js
+++ src/index.js
@@ -1,32 +1,91 @@
 import './sass/main.scss';
 import Notiflix from 'notiflix';
 import axios from 'axios';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const formRef = document.querySelector('#search-form');
+const loadMoreBtnRef = document.querySelector('.load-more');
 const galleryRef = document.querySelector('.gallery');
 
-formRef.addEventListener('submit', e => {
-  e.preventDefault();
-});
+formRef.addEventListener('submit', inputSearch);
+loadMoreBtnRef.addEventListener('click', onLoadMore);
 
-function markupPage() {
-  galleryRef.innerHTML = `<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
+let page = 1;
+let searchWords = '';
+
+async function fetchImages(searchWords) {
+  try {
+    const response = await axios.get(
+      `?key=26713885-825cc5fe55e7fb85d69d3b86a&image_type=photo&orientation=horizontal&safesearch=true&q=${searchWords}&per_page=40&page=${page}`,
+    );
+    if (response.status != 200) {
+      throw new Error('not found');
+    }
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+}
+
+async function inputSearch(e) {
+  galleryRef.innerHTML = '';
+  e.preventDefault();
+  searchWords = e.currentTarget.elements.searchQuery.value;
+  const images = await fetchImages(searchWords);
+  loadMoreBtnRef.classList.remove('is-hidden');
+
+  if (images.hits.length === 0) {
+    loadMoreBtnRef.classList.add('is-hidden');
+
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
+  renderPage(images);
+}
+
+async function onLoadMore() {
+  page += 1;
+  const images = await fetchImages(searchWords);
+  if (images.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
+  renderPage(images);
+}
+
+function markupCard(cards) {
+  let arrCards = [];
+  cards
+    .map(item => {
+      arrCards.push(`<div class="photo-card">
+  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="250"/>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
+      ${item.likes}
     </p>
     <p class="info-item">
-      <b>Views</b>
+      <b>Views</b>${item.views}
     </p>
     <p class="info-item">
       <b>Comments</b>
+      ${item.comments}
     </p>
     <p class="info-item">
-      <b>Downloads</b>
+      <b>Downloads</b>${item.downloads}
     </p>
   </div>
-</div>`;
+</div>`);
+    })
+    .join('');
+  return arrCards.join('');
+}
+
+function renderPage(cards) {
+  const markupCards = markupCard(cards.hits);
+  galleryRef.insertAdjacentHTML('beforeend', markupCards);
 }
 
 // markupPage();
